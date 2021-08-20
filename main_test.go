57e260f074e523b34f9b4e7c5cab864e1cc43020
tatamiya <tamyuji1002@gmail.com>
2021-08-20 18:17:25 +0900

    add integration tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package gcp_cost_alert
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -106,4 +107,64 @@ func TestCreateUnexpectedAlertDescriptionCorrectly(t *testing.T) {
 	}
 	actualAlertDescription := NewAlertDescription(&inputAlertPayload)
 	assert.EqualValues(t, expectedAlertDescription, actualAlertDescription)
+}
+
+type notifierStub struct {
+	SentMessage string
+	Err         error
+}
+
+func (n *notifierStub) Send(message string) error {
+	if n.Err != nil {
+		return n.Err
+	}
+	n.SentMessage = message
+	return nil
+}
+
+func TestRunWholeProcessCorrectly(t *testing.T) {
+	inputAlertPayload := PubSubData{
+		AlertThresholdExceeded: 1.0,
+		CostAmount:             100.01,
+		BudgetAmount:           100.00,
+		CurrencyCode:           "JPY",
+	}
+	testNotifier := notifierStub{}
+	expectedMessage := ":fire::fire::fire: 警告 :fire::fire::fire:\n:money_with_wings: GCP 利用額が 100.00 JPY を超過しました！（現在 100.01 JPY）"
+	err := alertNotification(&inputAlertPayload, &testNotifier)
+	actualMessage := testNotifier.SentMessage
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedMessage, actualMessage)
+}
+
+func TestReturnErrorWhenZeroAlertThresholdIsInput(t *testing.T) {
+	inputAlertPayload := PubSubData{
+		AlertThresholdExceeded: 0.0,
+		CostAmount:             0.00,
+		BudgetAmount:           0.00,
+		CurrencyCode:           "JPY",
+	}
+	testNotifier := notifierStub{}
+	err := alertNotification(&inputAlertPayload, &testNotifier)
+
+	assert.NotNil(t, err)
+	assert.EqualValues(
+		t,
+		"Unexpected AlertLevel with charged cost 0.00 JPY!",
+		err.Error(),
+	)
+}
+
+func TestReturnErrorWhenSlackNotificationFailed(t *testing.T) {
+	inputAlertPayload := PubSubData{
+		AlertThresholdExceeded: 1.0,
+		CostAmount:             100.01,
+		BudgetAmount:           100.00,
+		CurrencyCode:           "JPY",
+	}
+	testNotifier := notifierStub{Err: fmt.Errorf("Something Wrong!")}
+	err := alertNotification(&inputAlertPayload, &testNotifier)
+
+	assert.NotNil(t, err)
 }
