7fb7f3b3435321b73ea70249a7fe4c1ef0767d31
tatamiya <tamyuji1002@gmail.com>
2021-08-25 20:12:34 +0900

    add docs to payload struct

--- src/data/payload.go
+++ src/data/payload.go
@@ -1,5 +1,7 @@
 package data
 
+// The JSON object contained in the Data parts of a Pub/Sub message.
+// See also https://cloud.google.com/billing/docs/how-to/budgets-programmatic-notifications#notification_format
 type PubSubPayload struct {
 	AlertThresholdExceeded *float64 `json:"alertThresholdExceeded"`
 	CostAmount             float64  `json:"costAmount"`
