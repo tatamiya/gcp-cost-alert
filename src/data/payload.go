3bdf7f90b18e28097c72de358f8aae4c1be13de0
tatamiya <tamyuji1002@gmail.com>
2021-08-26 19:57:08 +0900

    add overview docs and docs discription

--- src/data/payload.go
+++ src/data/payload.go
@@ -1,7 +1,11 @@
+// The JSON payload of a Pub/Sub message.
 package data
 
-// The JSON object contained in the Data parts of a Pub/Sub message.
+// PubSubPayload is the JSON object contained in the Data parts of a Pub/Sub message.
 // See also https://cloud.google.com/billing/docs/how-to/budgets-programmatic-notifications#notification_format
+//
+// `AlertThresholdExceeded` is a float pointer
+// so that you get nil if the payload does not have this field.
 type PubSubPayload struct {
 	AlertThresholdExceeded *float64 `json:"alertThresholdExceeded"`
 	CostAmount             float64  `json:"costAmount"`
